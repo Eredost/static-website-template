cbd5c03c2a33542a32c5cbfcef5534bb565ec8a9
Eredost <michael.coutin62@gmail.com>
2021-08-23 10:23:50 +0200

    build: Sass preprocessor support in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         filename: 'js/[name].js',
         path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
     }
 }
